d0cb12d84a343df4b62675b844de19173195434d
James Robert Perih <james@hotdang.ca>
2017-03-16 19:04:22 -0600

    Use sqlite database and bootstrap

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app = express();
+const sqlite3 = require('sqlite3').verbose();
+
+const initDb = (() => {
+  const db = new sqlite3.Database('./db/pets.sqlite3');
+
+  db.serialize( () => {
+    db.run('CREATE TABLE IF NOT EXISTS pets(id INTEGER PRIMARY KEY, name TEXT, type TEXT, breed TEXT, location TEXT, latitude DOUBLE, longitude DOUBLE)');
+  });
+  console.log('db initialized');
+  db.close();
+});
+
+initDb();
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
